cd092615a17cdd373f05af4facd43517b49f0705
nrudrrr-ops <n.rudrrr@gmail.com>
2026-08-11 19:15:27 +0530

    Add Instagram and Resume links to footer

--- src/components/GuessTheBrand.jsx
+++ src/components/GuessTheBrand.jsx
@@ -1,4 +1,4 @@
-﻿import { useState, useMemo, useCallback, useEffect } from 'react';
+import { useState, useMemo, useCallback, useEffect } from 'react';
 import './GuessTheBrand.css';
 import CurtainCloth from './CurtainCloth';
 
